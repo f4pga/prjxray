8c143b3dd251945475e2a3bfd97af5829c3ca37a
Clifford Wolf <clifford@clifford.at>
2017-10-21 00:57:14 +0200

    Add more tags to ffconfig fuzzer (currently disabled)
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/011-ffconfig/generate.py
+++ fuzzers/011-ffconfig/generate.py
@@ -17,8 +17,21 @@ with open("design_%s.txt" % sys.argv[1], "r") as f:
         init = int(line[3][3])
         cinv = int(line[4][3])
 
+        if False:
+            segmk.addtag(site, "%s.TYPE_%s" % (bel, ctype), 1)
+
+            for i in range(1, 15):
+                types = set()
+                if i & 1: types.add("FDCE")
+                if i & 2: types.add("FDPE")
+                if i & 4: types.add("FDRE")
+                if i & 8: types.add("FDSE")
+                segmk.addtag(site, "%s.TYPES_%s" % (bel, "_".join(sorted(types))), ctype in types)
+
+        if False:
+            segmk.addtag(site, "%s.CLOCK_INV" % (bel.split(".")[0]), cinv)
+
         segmk.addtag(site, "%s.ZINI" % bel, 1-init)
-        # segmk.addtag(site, "%s.CLOCK_INV" % (bel.split(".")[0]), cinv)
 
 segmk.compile()
 segmk.write(sys.argv[1])
