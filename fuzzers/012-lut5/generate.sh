c265426f845e31b306ee349ae6105a50f6ed430f
John McMaster <JohnDMcMaster@gmail.com>
2017-11-10 15:56:42 -0800

    LUT5 vs LUT6 test
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ fuzzers/012-lut5/generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. ../../utils/genheader.sh
+
+echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
+
+vivado -mode batch -source ../generate.tcl
+
+for x in design*.bit; do
+	../../../tools/bitread -F $XRAY_ROI_FRAMES -o ${x}s -zy $x
+done
+
---
+++ fuzzers/012-lut5/generate.tcl
@@ -0,0 +1,88 @@
+create_project -force -part $::env(XRAY_PART) design design
+
+read_verilog ../top.v
+synth_design -top top
+
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_00) IOSTANDARD LVCMOS33" [get_ports clk]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_01) IOSTANDARD LVCMOS33" [get_ports di]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_02) IOSTANDARD LVCMOS33" [get_ports do]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_03) IOSTANDARD LVCMOS33" [get_ports stb]
+
+create_pblock roi
+add_cells_to_pblock [get_pblocks roi] [get_cells roi]
+resize_pblock [get_pblocks roi] -add "$::env(XRAY_ROI)"
+
+set_property CFGBVS VCCO [current_design]
+set_property CONFIG_VOLTAGE 3.3 [current_design]
+set_property BITSTREAM.GENERAL.PERFRAMECRC YES [current_design]
+
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
+
+place_design
+route_design
+
+write_checkpoint -force design.dcp
+write_bitstream -force design.bit
+
+
+
+
+
+# Get all 6LUT and 5LUT in pblock
+# A6LUT, B6LUT, A5LUT, etc
+set luts [get_bels -of_objects [get_sites -of_objects [get_pblocks roi]] -filter {TYPE =~ LUT*} */*LUT]
+
+
+
+set grid_min_x -1
+set grid_max_x -1
+set grid_min_y -1
+set grid_max_y -1
+
+foreach lut $luts {
+	set tile [get_tile -of_objects $lut]
+	set grid_x [get_property GRID_POINT_X $tile]
+	set grid_y [get_property GRID_POINT_Y $tile]
+
+	if [expr $grid_min_x < 0 || $grid_x < $grid_min_x] {set grid_min_x $grid_x}
+	if [expr $grid_max_x < 0 || $grid_x > $grid_max_x] {set grid_max_x $grid_x}
+
+	if [expr $grid_min_y < 0 || $grid_y < $grid_min_y] {set grid_min_y $grid_y}
+	if [expr $grid_max_y < 0 || $grid_y > $grid_max_y] {set grid_max_y $grid_y}
+}
+
+# set tiles [get_tiles -filter "GRID_POINT_X >= $grid_min_x && GRID_POINT_X <= $grid_max_x && GRID_POINT_Y >= $grid_min_y && GRID_POINT_Y <= $grid_max_y"]
+# set fp [open "tiles.txt" w]
+# foreach tile $tiles {
+# 	set type [get_property TYPE $tile]
+# 	set grid_x [get_property GRID_POINT_X $tile]
+# 	set grid_y [get_property GRID_POINT_Y $tile]
+# 	set sites [get_sites -quiet -of_objects $tile]
+# 	set typed_sites {}
+# 
+# 	if [llength $sites] {
+# 		set site_types [get_property SITE_TYPE $sites]
+# 		foreach t $site_types s $sites {
+# 			lappend typed_sites $t $s
+# 		}
+# 	}
+# 
+# 	puts $fp "$type $tile $grid_x $grid_y $typed_sites"
+# }
+# close $fp
+
+
+set fp [open "design.txt" w]
+foreach lut $luts {
+	set tile [get_tile -of_objects $lut]
+	set grid_x [get_property GRID_POINT_X $tile]
+	set grid_y [get_property GRID_POINT_Y $tile]
+	set type [get_property TYPE $tile]
+	set lut_type [get_property TYPE $lut]
+
+	if [get_property IS_USED $lut] {
+    	puts $fp "$type $tile $grid_x $grid_y $lut $lut_type"
+	}
+}
+close $fp
+
