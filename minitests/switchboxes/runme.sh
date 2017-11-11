61c51d6a1508826263106f2837e7f9a610fc05fb
Clifford Wolf <clifford@clifford.at>
2017-11-11 02:33:02 +0100

    Add segprint.py, use in switchboxes minitest
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/switchboxes/runme.sh
+++ minitests/switchboxes/runme.sh
@@ -2,3 +2,5 @@
 vivado -mode batch -source runme.tcl
 ../../tools/bitread -F $XRAY_ROI_FRAMES -o design.bits -zy design.bit
 ../../tools/bitread -F $XRAY_ROI_FRAMES -o routes.bits -zy routes.bit
+python3 ../../utils/segprint.py routes.bits INT_L_X12Y119 INT_L_X12Y117 INT_L_X12Y115
+python3 ../../utils/segprint.py routes.bits INT_R_X13Y118 INT_R_X13Y116 INT_R_X13Y114
--- utils/utils.tcl
+++ utils/utils.tcl
@@ -2,21 +2,26 @@
 proc route_via {net nodes} {
 	set net [get_nets $net]
 	set fixed_route [get_nodes -of_objects [get_site_pins -filter {DIRECTION == OUT} -of_objects $net]]
-
-	set nodes [get_nodes $nodes]
 	lappend nodes [get_nodes -of_objects [get_site_pins -filter {DIRECTION == IN} -of_objects $net]]
 
+	puts ""
+	puts "Routing net $net:"
+
 	foreach to_node $nodes {
+		set to_node [get_nodes -of_objects [get_wires $to_node]]
 		set from_node [lindex $fixed_route end]
 		set route [find_routing_path -quiet -from $from_node -to $to_node]
 		if {$route == ""} {
+			puts "  $from_node -> $to_node: no route found - assuming direct PIP"
 			lappend fixed_route $to_node
 		} {
+			puts "  $from_node -> $to_node: $route"
 			set fixed_route [concat $fixed_route [lrange $route 1 end]]
 		}
 	}
 
 	set_property FIXED_ROUTE $fixed_route $net
+	puts ""
 }
 
 proc putl {lst} {
