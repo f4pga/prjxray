67d461a1ed9f312b771fc755de7575e7faa49bf1
Clifford Wolf <clifford@clifford.at>
2017-11-12 17:31:22 +0100

    Add dbcheck.py, split seg_int.segbits
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ utils/dbcheck.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys, re
+
+database = dict()
+database_r = dict()
+
+for arg in sys.argv[1:]:
+    with open(arg, "r") as f:
+        for line in f:
+            if "<" in line:
+                raise Exception("Found '<' in this line: %s" % line)
+
+            line = line.split()
+            key = line[0]
+            bits = tuple(sorted(set(line[1:])))
+
+            if key in database:
+                raise Exception("Duplicate key: %s %s" % (key, bits))
+
+            if bits in database_r:
+                raise Exception("Duplicate bits: %s %s" % (key, bits))
+
+            database[key] = bits
+            database_r[bits] = key
+
+def get_subsets(bits):
+    retval = list()
+    retval.append(bits)
+    for i in range(len(bits)):
+        for s in get_subsets(bits[i+1:]):
+            retval.append(bits[0:i] + s);
+    return retval
+
+def check_subsets(bits):
+    for sub_bits in sorted(get_subsets(bits)):
+        if sub_bits != bits and sub_bits != ():
+            if sub_bits in database_r:
+                raise Exception("Entry %s %s is a subset of entry %s %s." % (database_r[sub_bits], sub_bits, database_r[bits], bits))
+
+for key, bits in database.items():
+    check_subsets(bits)
+
