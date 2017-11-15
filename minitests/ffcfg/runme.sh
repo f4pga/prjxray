a2f4ab208202772a0a5d13a57b202905991de587
Rick Altherr <kc8apf@kc8apf.net>
2017-11-15 11:35:55 -0500

    minitests: fix tool paths
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/ffcfg/runme.sh
+++ minitests/ffcfg/runme.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ../../utils/environment.sh
-
 set -ex
 vivado -mode batch -source runme.tcl
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
--- minitests/switchboxes/runme.sh
+++ minitests/switchboxes/runme.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ../../utils/environment.sh
-
 vivado -mode batch -source runme.tcl
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o routes.bits -z -y routes.bit
