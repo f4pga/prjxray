1f7b3ff2267f26275110b9db0790ac904e86426e
Rick Altherr <kc8apf@kc8apf.net>
2017-11-14 16:08:07 -0800

    Avoid combined flags in bitread invocations.
    Combined flags (i.e. -zy) are not universally supported.  Switch to
    individual flags (-z -y) in existing scripts to prepare for move to
    gflags.
    
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/011-ffconfig/generate.sh
+++ fuzzers/011-ffconfig/generate.sh
@@ -7,7 +7,7 @@ echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
 vivado -mode batch -source ../generate.tcl
 
 for i in {0..9}; do
-	../../../tools/bitread -F $XRAY_ROI_FRAMES -o design_$i.bits -zy design_$i.bit
+	../../../tools/bitread -F $XRAY_ROI_FRAMES -o design_$i.bits -z -y design_$i.bit
 done
 
 for i in {0..9}; do
--- fuzzers/013-intpips/generate.sh
+++ fuzzers/013-intpips/generate.sh
@@ -6,6 +6,6 @@ echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
 
 vivado -mode batch -source ../generate.tcl
 
-../../../tools/bitread -F $XRAY_ROI_FRAMES -o design.bits -zy design.bit
+../../../tools/bitread -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
 python3 ../generate.py
 
