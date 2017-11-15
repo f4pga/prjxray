073499a784cb0a2ccc844d8a0585904ea11589a5
Rick Altherr <kc8apf@kc8apf.net>
2017-11-15 11:35:55 -0500

    minitests: fix tool paths
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/lvb_long_mux/runme.sh
+++ minitests/lvb_long_mux/runme.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-source ../../utils/environment.sh
-
 set -ex
 vivado -mode batch -source runme.tcl
-${XRAY_BITREAD}-F $XRAY_ROI_FRAMES -o design_a.bits -z -y design_a.bit
-${XRAY_BITREAD}-F $XRAY_ROI_FRAMES -o design_b.bits -z -y design_b.bit
+${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_a.bits -z -y design_a.bit
+${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_b.bits -z -y design_b.bit
 ${XRAY_SEGPRINT} design_a.bits INT_L_X12Y132 INT_L_X14Y132 INT_L_X16Y132
 ${XRAY_SEGPRINT} design_b.bits INT_L_X12Y132 INT_L_X14Y132 INT_L_X16Y132
