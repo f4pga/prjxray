559eb4176a76aa7d5a601b60093e90f3ba26f08a
Rick Altherr <kc8apf@kc8apf.net>
2017-11-15 01:57:50 -0500

    Export XRAY_* during environment setup
    Since some of the tools need to be found by make and other environments,
    build the paths when database/<database>/settings.sh is sourced and
    export them into the shell environment.
    
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- utils/environment.sh
+++ utils/environment.sh
@@ -4,13 +4,14 @@ while [ -h "$XRAY_ENV_PATH" ]; do # resolve $XRAY_ENV_PATH until the file is no 
   XRAY_ENV_PATH="$(readlink "$XRAY_ENV_PATH")"
   [[ $XRAY_ENV_PATH != /* ]] && XRAY_ENV_PATH="$XRAY_UTILS_DIR/$XRAY_ENV_PATH" # if $XRAY_ENV_PATH was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-XRAY_UTILS_DIR="$( cd -P "$( dirname "$XRAY_ENV_PATH" )" && pwd )"
+export XRAY_UTILS_DIR="$( cd -P "$( dirname "$XRAY_ENV_PATH" )" && pwd )"
 
-XRAY_DIR="$( dirname "$XRAY_UTILS_DIR" )"
-XRAY_DATABASE_DIR="${XRAY_DIR}/database"
-XRAY_TOOLS_DIR="${XRAY_DIR}/build/tools"
+export XRAY_DIR="$( dirname "$XRAY_UTILS_DIR" )"
+export XRAY_DATABASE_DIR="${XRAY_DIR}/database"
+export XRAY_TOOLS_DIR="${XRAY_DIR}/build/tools"
 
-XRAY_GENHEADER="${XRAY_UTILS_DIR}/genheader.sh"
-XRAY_BITREAD="${XRAY_TOOLS_DIR}/bitread"
-XRAY_SEGMATCH="${XRAY_TOOLS_DIR}/segmatch"
-XRAY_SEGPRINT="python2 ${XRAY_UTILS_DIR}/segprint.py"
+export XRAY_GENHEADER="${XRAY_UTILS_DIR}/genheader.sh"
+export XRAY_BITREAD="${XRAY_TOOLS_DIR}/bitread"
+export XRAY_MERGEDB="bash ${XRAY_UTILS_DIR}/mergedb.sh"
+export XRAY_SEGMATCH="${XRAY_TOOLS_DIR}/segmatch"
+export XRAY_SEGPRINT="python2 ${XRAY_UTILS_DIR}/segprint.py"
