5fe0d49eb58bf1fd1ecd60e83780bfd9ea00ffb9
John McMaster <JohnDMcMaster@gmail.com>
2017-11-17 18:01:09 -0800

    ffcfg: comment out broken call
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/ffcfg/runme.sh
+++ minitests/ffcfg/runme.sh
@@ -3,4 +3,4 @@
 set -ex
 vivado -mode batch -source runme.tcl
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
-${XRAY_SEGPRINT} design.bits SLICE_X16Y100 SLICE_X16Y101 SLICE_X16Y102 SLICE_X16Y103
+#${XRAY_SEGPRINT} design.bits SLICE_X16Y100 SLICE_X16Y101 SLICE_X16Y102 SLICE_X16Y103
