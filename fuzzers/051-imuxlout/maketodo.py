324d9d1c8be3cf089eb94af9eef1ce2e9e017b45
Clifford Wolf <clifford@clifford.at>
2017-11-18 21:19:14 +0100

    Add fuzzers/051-imuxlout
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ fuzzers/051-imuxlout/maketodo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os, re
+
+def maketodo(pipfile, dbfile):
+    todos = set()
+    with open(pipfile, "r") as f:
+        for line in f:
+            line = line.split()
+            todos.add(line[0])
+    with open(dbfile, "r") as f:
+        for line in f:
+            line = line.split()
+            todos.remove(line[0])
+    for line in todos:
+        if re.match(r"^INT_[LR].IMUX(_L)?[0-9]+\.LOGIC_OUTS(_L)?[0-9]+$", line):
+            print(line)
+
+maketodo("pips_int_l.txt", "%s/%s/segbits_int_l.db" % (os.getenv("XRAY_DATABASE_DIR"), os.getenv("XRAY_DATABASE")))
+maketodo("pips_int_r.txt", "%s/%s/segbits_int_r.db" % (os.getenv("XRAY_DATABASE_DIR"), os.getenv("XRAY_DATABASE")))
+
