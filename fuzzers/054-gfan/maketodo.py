40894fcb0039da4b1db1d61320b1e654ab6eb1d8
Clifford Wolf <clifford@clifford.at>
2017-11-19 23:14:49 +0000

    Update database
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/054-gfan/maketodo.py
+++ fuzzers/054-gfan/maketodo.py
@@ -13,7 +13,7 @@ def maketodo(pipfile, dbfile):
             line = line.split()
             todos.remove(line[0])
     for line in todos:
-        if re.match(r"^INT_[LR].GFAN", line):
+        if re.match(r"^INT_[LR].GFAN", line) and not line.endswith(".GND_WIRE"):
             print(line)
 
 maketodo("pips_int_l.txt", "%s/%s/segbits_int_l.db" % (os.getenv("XRAY_DATABASE_DIR"), os.getenv("XRAY_DATABASE")))
