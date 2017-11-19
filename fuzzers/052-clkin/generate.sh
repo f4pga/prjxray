8eee91472ebae56e6905452290e407f27a6f7dc5
Clifford Wolf <clifford@clifford.at>
2017-11-19 06:55:22 +0000

    Update database
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/052-clkin/generate.sh
+++ fuzzers/052-clkin/generate.sh
@@ -5,5 +5,5 @@ source ${XRAY_GENHEADER}
 vivado -mode batch -source ../generate.tcl
 
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
-python3 ../../050-intpips/generate.py
+python3 ../generate.py
 
