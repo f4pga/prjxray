4df3a9bce79223f5f048b393eb7e7e7f6acc7aea
Clifford Wolf <clifford@clifford.at>
2017-11-19 08:14:31 +0000

    Update database
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/052-clkin/generate.py
+++ fuzzers/052-clkin/generate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, os, re
 
 sys.path.append("../../../utils/")
 from segmaker import segmaker
@@ -58,6 +58,14 @@ for tile, pips_srcs_dsts in tiledata.items():
         elif src_dst[1] not in dsts:
             segmk.addtag(tile, "%s.%s" % (dst, src), 0)
 
-segmk.compile()
+def bitfilter(frame_idx, bit_idx):
+    assert os.getenv("XRAY_DATABASE") == "artix7"
+    if frame_idx == 0 and bit_idx == 48:
+        return False
+    if frame_idx == 1 and bit_idx == 31:
+        return False
+    return frame_idx in [0, 1]
+
+segmk.compile(bitfilter=bitfilter)
 segmk.write()
 
