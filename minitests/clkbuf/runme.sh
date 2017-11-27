6e70ca80bb587f2bb1e108ff75926092f259aa38
Clifford Wolf <clifford@clifford.at>
2017-11-27 01:02:48 +0100

    Add "segprint -b"
    Signed-off-by: Clifford Wolf <clifford@clifford.at>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/clkbuf/runme.sh
+++ minitests/clkbuf/runme.sh
@@ -3,10 +3,10 @@
 set -ex
 vivado -mode batch -source runme.tcl
 
-${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
-${XRAY_SEGPRINT} -zd design.bits > design.segs
+${XRAY_BITREAD} -o design.bits -z -y design.bit
+${XRAY_SEGPRINT} -bzd design.bits > design.segs
 
 for id in b{0,1,2,3,4,5,6,7,8,9,10,11}; do
-	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_$id.bits -z -y design_$id.bit
-	${XRAY_SEGPRINT} -zd design_$id.bits > design_$id.segs
+	${XRAY_BITREAD} -o design_$id.bits -z -y design_$id.bit
+	${XRAY_SEGPRINT} -bzd design_$id.bits > design_$id.segs
 done
