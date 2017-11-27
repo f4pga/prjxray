6eb44b0c33f761723d7436cd2c95ec96841bc6eb
John McMaster <JohnDMcMaster@gmail.com>
2017-11-27 15:43:02 -0800

    fixedpnr: include latches
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/fixedpnr/runme.sh
+++ minitests/fixedpnr/runme.sh
@@ -2,9 +2,9 @@
 
 set -ex
 vivado -mode batch -source runme.tcl
-for ff in fdre fdse fdce fdce_inv fdpe; do
+for ff in fdre fdse fdce fdce_inv fdpe ldce ldpe; do
     ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_$ff.bits -z -y design_$ff.bit
-    ${XRAY_SEGPRINT} design_$ff.bits >design_$ff.seg
+    ${XRAY_SEGPRINT} -z design_$ff.bits >design_$ff.seg
 done
 
 diff -u design_fdre.bits design_fdse.bits
