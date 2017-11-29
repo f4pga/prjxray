188d969af7b77af47bbf5cc04480915c02ca7a58
John McMaster <JohnDMcMaster@gmail.com>
2017-11-28 16:25:39 -0800

    fixedpnr: fix latch INV/INIT polarity
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- minitests/fixedpnr/runme.sh
+++ minitests/fixedpnr/runme.sh
@@ -7,4 +7,17 @@ for ff in fdre fdse fdce fdce_inv fdpe ldce ldpe; do
     ${XRAY_SEGPRINT} -z design_$ff.bits >design_$ff.seg
 done
 
-diff -u design_fdre.bits design_fdse.bits
+# Clock inverter bit
+diff design_fdce.seg design_fdce_inv.seg || true
+# Bits set on FF's are a superset of FDPE
+# FDSE has the most bits set
+diff design_fdpe.seg design_fdse.seg || true
+diff design_fdpe.seg design_fdce.seg || true
+diff design_fdpe.seg design_fdre.seg || true
+
+# the latch bit
+diff design_fdpe.seg design_ldpe.seg || true
+# LDPE has one more bit pair set than LDCE
+# This is the same pair FDRE/LDCE have
+diff design_ldpe.seg design_ldce.seg || true
+
