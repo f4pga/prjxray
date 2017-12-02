7c2500eee7de4af1a59771fd540efbcb4e0b0407
John McMaster <JohnDMcMaster@gmail.com>
2017-12-01 16:20:13 -0800

    clbn5ffmux fuzzer
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ fuzzers/012-clbn5ffmux/generate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys, re
+
+sys.path.append("../../../utils/")
+from segmaker import segmaker
+
+segmk = segmaker("design.bits")
+
+print("Loading tags")
+'''
+module,loc,n,def_a
+clb_N5FFMUX,SLICE_X12Y100,3,1
+clb_N5FFMUX,SLICE_X13Y100,0,1
+clb_N5FFMUX,SLICE_X14Y100,3,1
+'''
+f = open('params.csv', 'r')
+f.readline()
+for l in f:
+    module,loc,n,def_a = l.split(',')
+    def_a = int(def_a)
+    n = int(n)
+    #which = chr(ord('A') + n)
+
+    for i, which in enumerate('ABCD'):
+        # Theory: there is one bit for each mux positon
+        # In each config 3 muxes are in one position, other 3 are in another
+        inv = int(i == n)
+        segmk.addtag(loc, "%c5FF.MUX.A" % which, def_a ^ inv)
+        segmk.addtag(loc, "%c5FF.MUX.B" % which, 1 ^ def_a ^ inv)
+segmk.compile()
+segmk.write()
+
---
+++ fuzzers/012-clbn5ffmux/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+. ../../utils/genheader.sh
+
+#echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
+
+python3 ../top.py >top.v
+vivado -mode batch -source ../generate.tcl
+
+for x in design*.bit; do
+	../../../build/tools/bitread -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
+done
+
+python3 ../generate.py
+
