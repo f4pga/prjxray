9f1e936ab0eb844d309cf8df2a3f83b33fe4fa7d
Rick Altherr <kc8apf@kc8apf.net>
2017-12-04 11:02:55 -0800

    bitops: setter and add const
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- lib/include/prjxray/bit_ops.h
+++ lib/include/prjxray/bit_ops.h
@@ -4,7 +4,7 @@
 namespace prjxray {
 
 template<typename UInt>
-constexpr UInt bit_mask(int bit) {
+constexpr UInt bit_mask(const int bit) {
 	return (static_cast<UInt>(1) << bit);
 }
 
@@ -19,7 +19,7 @@ constexpr UInt bit_all_ones() {
 }
 
 template<typename UInt>
-constexpr UInt bit_mask_range(int top_bit, int bottom_bit) {
+constexpr UInt bit_mask_range(const int top_bit, const int bottom_bit) {
 	return ((bit_all_ones<UInt>() >> (bit_sizeof<UInt>() - 1 - top_bit)) &
 		(bit_all_ones<UInt>() - bit_mask<UInt>(bottom_bit) +
 		 static_cast<UInt>(1)));
@@ -27,8 +27,17 @@ constexpr UInt bit_mask_range(int top_bit, int bottom_bit) {
 
 
 template<typename UInt>
-constexpr UInt bit_field_get(UInt value, int top_bit, int bottom_bit) {
+constexpr UInt bit_field_get(UInt value, const int top_bit, const int bottom_bit) {
 	return (value & bit_mask_range<UInt>(top_bit, bottom_bit)) >> bottom_bit;
+}
+
+template<typename UInt>
+constexpr UInt bit_field_set(const UInt reg_value,
+			     const int top_bit, const int bottom_bit,
+			     const UInt field_value) {
+	return ((reg_value & ~bit_mask_range<UInt>(top_bit, bottom_bit)) |
+		((static_cast<UInt>(field_value) << bottom_bit) &
+		 bit_mask_range<UInt>(top_bit, bottom_bit)));
 }
 
 }  // namespace prjxray
