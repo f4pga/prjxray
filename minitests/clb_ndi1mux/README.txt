c9d65e18132f62301154bf38b207354209fcd028
John McMaster <JohnDMcMaster@gmail.com>
2017-12-11 13:41:28 -0800

    clb_ndi1mux minitest
    Signed-off-by: John McMaster <JohnDMcMaster@gmail.com>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ minitests/clb_ndi1mux/README.txt
@@ -0,0 +1,12 @@
+Trying to set SLICEM LUT DI1 inputs
+These exist for LUTA, LUTB, and LUTC only
+Can either be an external signal, another LUT's data input, or another LUT's carry
+Note: mux input pattern is irregular
+
+Result:
+Neither external input nor carry input set any unknown bits
+Unclear what is going on
+Maybe an earlier test incorrectly set these?
+Additionally, I could not get BDI1 to activate
+Maybe should do a closer pass on BI/DI, which may be easier to trigger
+
---
+++ minitests/clb_ndi1mux/top.v
@@ -0,0 +1,326 @@
+/*
+SLICEM at the following:
+SLICE_XxY*
+Where Y any value
+x
+    Always even (ie 100, 102, 104, etc)
+    In our ROI
+    x = 6, 8, 10, 12, 14
+
+SRL16E: LOC + BEL
+SRLC32E: LOC + BEL
+RAM64X1S: LOCs but doesn't BEL
+*/
+
+module top(input clk, stb, di, output do);
+	localparam integer DIN_N = 256;
+	localparam integer DOUT_N = 256;
+
+	reg [DIN_N-1:0] din;
+	wire [DOUT_N-1:0] dout;
+
+	reg [DIN_N-1:0] din_shr;
+	reg [DOUT_N-1:0] dout_shr;
+
+	always @(posedge clk) begin
+		din_shr <= {din_shr, di};
+		dout_shr <= {dout_shr, din_shr[DIN_N-1]};
+		if (stb) begin
+			din <= din_shr;
+			dout_shr <= dout;
+		end
+	end
+
+	assign do = dout_shr[DOUT_N-1];
+
+	roi roi (
+		.clk(clk),
+		.din(din),
+		.dout(dout)
+	);
+endmodule
+
+module roi(input clk, input [255:0] din, output [255:0] dout);
+`define ALL1
+`ifdef ALL1
+    //ok
+    my_NDI1MUX_NMC31 #(.LOC("SLICE_X6Y100"))
+            my_NDI1MUX_NMC31(.clk(clk), .din(din[  0 +: 8]), .dout(dout[  0 +: 8]));
+    /*
+    //Can't find a valid solution
+    my_NDI1MUX_NDI1 #(.LOC("SLICE_X6Y101"))
+            my_NDI1MUX_NDI1(.clk(clk), .din(din[  8 +: 32]), .dout(dout[  8 +: 8]));
+    */
+    my_NDI1MUX_NI #(.LOC("SLICE_X6Y102"))
+            my_NDI1MUX_NI(.clk(clk), .din(din[  40 +: 8]), .dout(dout[  40 +: 8]));
+`endif
+
+`define SINGLE1
+`ifdef SINGLE1
+    //ok
+    my_BDI1MUX_AI #(.LOC("SLICE_X8Y100"), .BEL("A6LUT"))
+            my_BDI1MUX_AI(.clk(clk), .din(din[  64 +: 8]), .dout(dout[  64 +: 8]));
+    /*
+    //bad
+    my_BDI1MUX_BDI1 #(.LOC("SLICE_X8Y101"), .BELO("C6LUT"), .BELI("A6LUT"))
+            my_BDI1MUX_BDI1(.clk(clk), .din(din[  72 +: 8]), .dout(dout[  72 +: 8]));
+    */
+    //ok
+    my_BDI1MUX_BMC31 #(.LOC("SLICE_X8Y102"), .BELO("B6LUT"), .BELI("A6LUT"))
+            my_BDI1MUX_BMC31(.clk(clk), .din(din[  80 +: 8]), .dout(dout[  80 +: 8]));
+`endif
+endmodule
+
+
+/****************************************************************************
+Tries to set all three muxes at once
+****************************************************************************/
+
+module my_NDI1MUX_NMC31 (input clk, input [7:0] din, output [7:0] dout);
+    parameter LOC = "SLICE_X6Y100";
+    wire [3:0] q31;
+
+    (* LOC=LOC, BEL="D6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutd (
+            .Q(dout[0]),
+            .Q31(q31[3]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL="C6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutc (
+            .Q(dout[1]),
+            .Q31(q31[2]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            //.D(din[7]));
+            .D(q31[3]));
+    (* LOC=LOC, BEL="B6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutb (
+            .Q(dout[2]),
+            .Q31(q31[1]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            //.D(din[7]));
+            .D(q31[2]));
+    (* LOC=LOC, BEL="A6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) luta (
+            .Q(dout[3]),
+            .Q31(q31[0]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            //.D(din[7]));
+            .D(q31[1]));
+endmodule
+
+/*
+//Cannot loc instance 'roi/my_NDI1MUX_NDI1/lutc' at site SLICE_X6Y100,
+//Bel does not match with the valid locations at which this inst can be placed
+
+module my_NDI1MUX_NDI1 (input clk, input [31:0] din, output [7:0] dout);
+    parameter LOC = "SLICE_X6Y100";
+    wire [3:0] q31;
+
+    (* LOC=LOC, BEL="D6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutd (
+            .Q(dout[0]),
+            .Q31(q31[3]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL="C6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutc (
+            .Q(dout[1]),
+            .Q31(q31[2]),
+            .A(din[12:8]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[15]));
+    (* LOC=LOC, BEL="B6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutb (
+            .Q(dout[2]),
+            .Q31(q31[1]),
+            .A(din[20:16]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            //.D(din[23]));
+            .D(q31[2]));
+    (* LOC=LOC, BEL="A6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) luta (
+            .Q(dout[3]),
+            .Q31(q31[0]),
+            .A(din[28:24]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            //.D(din[31]));
+            .D(q31[2]));
+endmodule
+*/
+
+
+module my_NDI1MUX_NI (input clk, input [7:0] din, output [7:0] dout);
+    parameter LOC = "SLICE_X6Y100";
+
+    (* LOC=LOC, BEL="D6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutd (
+            .Q(dout[0]),
+            .Q31(),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL="C6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutc (
+            .Q(dout[1]),
+            .Q31(),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL="B6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutb (
+            .Q(dout[2]),
+            .Q31(),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL="A6LUT" *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) luta (
+            .Q(dout[3]),
+            .Q31(),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+endmodule
+
+/****************************************************************************
+Individual mux tests
+****************************************************************************/
+
+module my_BDI1MUX_AI (input clk, input [7:0] din, output [7:0] dout);
+    parameter LOC = "";
+    parameter BEL="A6LUT";
+
+    wire mc31c;
+
+    (* LOC=LOC, BEL=BEL *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lut (
+            .Q(dout[0]),
+            .Q31(mc31c),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+endmodule
+
+module my_BDI1MUX_BDI1 (input clk, input [7:0] din, output [7:0] dout);
+    parameter LOC = "";
+    parameter BELO="C6LUT";
+    parameter BELI="A6LUT";
+
+    wire mc31c;
+    //wire da = din[6];
+
+    (* LOC=LOC, BEL=BELO *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutb (
+            .Q(dout[0]),
+            .Q31(mc31c),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL=BELI *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) luta (
+            .Q(dout[1]),
+            .Q31(dout[2]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(mc31c));
+endmodule
+
+//ok
+module my_BDI1MUX_BMC31 (input clk, input [7:0] din, output [7:0] dout);
+    parameter LOC = "";
+    parameter BELO="B6LUT";
+    parameter BELI="A6LUT";
+
+    wire mc31b;
+
+    (* LOC=LOC, BEL=BELO *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) lutb (
+            .Q(dout[0]),
+            .Q31(mc31b),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(din[7]));
+    (* LOC=LOC, BEL=BELI *)
+    SRLC32E #(
+            .INIT(32'h00000000),
+            .IS_CLK_INVERTED(1'b0)
+        ) luta (
+            .Q(dout[1]),
+            .Q31(dout[2]),
+            .A(din[4:0]),
+            .CE(din[5]),
+            .CLK(din[6]),
+            .D(mc31b));
+
+endmodule
+
