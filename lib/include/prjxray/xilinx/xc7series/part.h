d251aa0ed7bcd01dd23c75d15f36f14b6cde7264
Rick Altherr <kc8apf@kc8apf.net>
2017-12-15 13:04:38 -0800

    lib: xc7series: wrapper for part-specific information
    Currently just the pieces required to successfully identify and decode
    a bitstream into configuration frames.
    
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ lib/include/prjxray/xilinx/xc7series/part.h
@@ -0,0 +1,55 @@
+#ifndef PRJXRAY_LIB_XILINX_XC7SERIES_PART_H_
+#define PRJXRAY_LIB_XILINX_XC7SERIES_PART_H_
+
+#include <vector>
+
+#include <absl/types/optional.h>
+#include <prjxray/xilinx/xc7series/configuration_frame_address.h>
+#include <prjxray/xilinx/xc7series/configuration_frame_range.h>
+
+namespace prjxray {
+namespace xilinx {
+namespace xc7series {
+
+class Part {
+ public:
+	static absl::optional<Part> FromFile(const std::string &path);
+
+	// Constructs an invalid part with a zero IDCODE. Required for YAML
+	// conversion but shouldn't be used otherwise.
+	Part() : idcode_(0), frame_ranges_() {}
+
+	Part(uint32_t idcode,
+	     const std::vector<ConfigurationFrameRange> &ranges)
+	: idcode_(idcode), frame_ranges_(std::move(ranges)) {}
+
+	uint32_t idcode() const { return idcode_; }
+
+	const std::vector<ConfigurationFrameRange>&
+	configuration_frame_ranges() const { return frame_ranges_; }
+
+	absl::optional<ConfigurationFrameAddress>
+	GetNextConfigurationFrameAddress(
+			ConfigurationFrameAddress address) const;
+
+ private:
+  uint32_t idcode_;
+
+  // Ranges are expected to be non-overlapping.
+  std::vector<ConfigurationFrameRange> frame_ranges_;
+};
+
+}  // namespace xc7series
+}  // namespace xilinx
+}  // namespace prjxray
+
+namespace YAML {
+template<>
+struct convert<prjxray::xilinx::xc7series::Part> {
+	static Node encode(const prjxray::xilinx::xc7series::Part &rhs);
+	static bool decode(const Node& node,
+			   prjxray::xilinx::xc7series::Part &lhs);
+};
+} // namespace YAML
+
+#endif // PRJXRAY_LIB_XILINX_XC7SERIES_PART_H_
---
+++ lib/xilinx/xc7series/part.cc
@@ -0,0 +1,82 @@
+#include <prjxray/xilinx/xc7series/part.h>
+
+#include <iomanip>
+#include <sstream>
+
+namespace prjxray {
+namespace xilinx {
+namespace xc7series {
+
+absl::optional<Part> Part::FromFile(const std::string &path) {
+	try {
+		YAML::Node yaml = YAML::LoadFile(path);
+		return yaml.as<Part>();
+	} catch (YAML::Exception e) {
+		return {};
+	}
+}
+
+absl::optional<ConfigurationFrameAddress>
+Part::GetNextConfigurationFrameAddress(ConfigurationFrameAddress address) const {
+	// Start with the next linear address.
+	ConfigurationFrameAddress target_address(address + 1);
+
+	// The address space is non-continguous.  If the next linear address
+	// happens to fall in a valid range, that's the next address.
+	// Otherwise, find the closest valid range and use it's beginning
+	// address.
+	absl::optional<ConfigurationFrameAddress> closest_address;
+	int32_t closest_distance;
+	for (auto iter = frame_ranges_.begin();
+	     iter != frame_ranges_.end();
+	     ++iter) {
+		if (iter->Contains(target_address)) {
+			return target_address;
+		}
+
+		int32_t distance = iter->begin() - target_address;
+		if (distance > 0 &&
+		    (!closest_address ||
+		     distance < closest_distance)) {
+			closest_address = iter->begin();
+			closest_distance = distance;
+		}
+	}
+
+	return closest_address;
+}
+
+}  // namespace xc7series
+}  // namespace xilinx
+}  // namespace prjxray
+
+namespace xc7series = prjxray::xilinx::xc7series;
+
+namespace YAML {
+
+Node convert<xc7series::Part>::encode(const xc7series::Part &rhs) {
+	Node node;
+	node.SetTag("xilinx/xc7series/part");
+
+	std::ostringstream idcode_str;
+	idcode_str << "0x" << std::hex << rhs.idcode();
+	node["idcode"] = idcode_str.str();
+
+	node["configuration_ranges"] = rhs.configuration_frame_ranges();
+	return node;
+}
+
+bool convert<xc7series::Part>::decode(const Node &node, xc7series::Part &lhs) {
+	if (node.Tag() != "xilinx/xc7series/part" ||
+	    !node["idcode"] ||
+	    !node["configuration_ranges"]) return false;
+
+	lhs = xc7series::Part(
+		node["idcode"].as<uint32_t>(),
+		node["configuration_ranges"].as<
+			std::vector<xc7series::ConfigurationFrameRange>>());
+	return true;
+}
+
+}  // namespace YAML;
+
