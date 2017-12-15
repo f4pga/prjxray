6320575314726485d4d222196f2d7bb90e29a27c
Rick Altherr <kc8apf@kc8apf.net>
2017-12-15 13:04:38 -0800

    lib: xc7series: wrapper for part-specific information
    Currently just the pieces required to successfully identify and decode
    a bitstream into configuration frames.
    
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

---
+++ lib/xilinx/xc7series/part_test.cc
@@ -0,0 +1,40 @@
+#include <prjxray/xilinx/xc7series/part.h>
+
+#include <gtest/gtest.h>
+
+namespace xc7series = prjxray::xilinx::xc7series;
+
+TEST(PartTest, GetNextAddressWhereNextIsInValidRange) {
+	std::vector<xc7series::ConfigurationFrameRange> ranges;
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x0, 0xF));
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x20, 0x2F));
+
+	xc7series::Part part(0x1234, ranges);
+
+	auto next_address = part.GetNextConfigurationFrameAddress(0x4);
+	EXPECT_TRUE(next_address);
+	EXPECT_EQ(static_cast<uint32_t>(0x5), *next_address);
+}
+
+TEST(PartTest, GetNextAddressWhereNextIsBetweenRanges) {
+	std::vector<xc7series::ConfigurationFrameRange> ranges;
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x0, 0xF));
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x20, 0x2F));
+
+	xc7series::Part part(0x1234, ranges);
+
+	auto next_address = part.GetNextConfigurationFrameAddress(0xF);
+	EXPECT_TRUE(next_address);
+	EXPECT_EQ(static_cast<uint32_t>(0x20), *next_address);
+}
+
+TEST(PartTest, GetNextAddressWhereNextWouldBePastLastRange) {
+	std::vector<xc7series::ConfigurationFrameRange> ranges;
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x0, 0xF));
+	ranges.push_back(xc7series::ConfigurationFrameRange(0x20, 0x2F));
+
+	xc7series::Part part(0x1234, ranges);
+
+	auto next_address = part.GetNextConfigurationFrameAddress(0x2F);
+	EXPECT_FALSE(next_address);
+}
