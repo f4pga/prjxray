955360a8ed6aac20551cea0fe2320b3025f45648
Rick Altherr <kc8apf@kc8apf.net>
2017-12-18 15:35:22 -0800

    fuzzers: use env vars to refer to tools
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>
    Signed-off-by: Tim 'mithro' Ansell <mithro@mithis.com>

--- fuzzers/011-ffconfig/generate.sh
+++ fuzzers/011-ffconfig/generate.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-. ../../utils/genheader.sh
+source ${XRAY_GENHEADER}
 
 #echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
 
@@ -10,7 +10,7 @@ python3 ../top.py >top.v
 vivado -mode batch -source ../generate.tcl
 
 for x in design*.bit; do
-	../../../build/tools/bitread -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
+	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
 done
 
 python3 ../generate.py
