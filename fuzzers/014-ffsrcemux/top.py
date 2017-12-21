b9ede7cab9d7fc1243bfab8b7c91af8683549e9d
Clifford Wolf <clifford@clifford.at>
2017-12-21 01:49:34 +0100

    Fix various instances of top.py for kintex ROI
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- fuzzers/014-ffsrcemux/top.py
+++ fuzzers/014-ffsrcemux/top.py
@@ -7,7 +7,7 @@ def slice_xy():
     '''Return (X1, X2), (Y1, Y2) from XRAY_ROI, exclusive end (for xrange)'''
     # SLICE_X12Y100:SLICE_X27Y149
     # Note XRAY_ROI_GRID_* is something else
-    m = re.match(r'SLICE_X(.*)Y(.*):SLICE_X(.*)Y(.*)', os.getenv('XRAY_ROI'))
+    m = re.match(r'SLICE_X([0-9]*)Y([0-9]*):SLICE_X([0-9]*)Y([0-9]*)', os.getenv('XRAY_ROI'))
     ms = [int(m.group(i + 1)) for i in range(4)]
     return ((ms[0], ms[2] + 1), (ms[1], ms[3] + 1))
 
