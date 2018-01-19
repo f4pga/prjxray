169a822865a603c43fcb779ba2e87b10694630a5
John McMaster <johndmcmaster@gmail.com>
2018-01-18 19:53:25 -0800

    roi_harness: basys3 pins to allow partgen
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- minitests/roi_harness/basys3.sh
+++ minitests/roi_harness/basys3.sh
@@ -2,3 +2,12 @@
 export XRAY_PART=xc7a35tcpg236-1
 export XRAY_PINCFG=BASYS3-SWBUT
 
+# For generating DB
+export XRAY_PIN_00="V17"
+export XRAY_PIN_01="V16"
+export XRAY_PIN_02="W16"
+export XRAY_PIN_03="W17"
+export XRAY_PIN_04="W15"
+export XRAY_PIN_05="V15"
+export XRAY_PIN_06="W14"
+
