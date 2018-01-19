4237ed08984d44c6397375b30babc02c79ea3568
John McMaster <johndmcmaster@gmail.com>
2018-01-18 20:25:42 -0800

    roi_harness: generate .frm file
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- minitests/roi_harness/runme.sh
+++ minitests/roi_harness/runme.sh
@@ -9,4 +9,5 @@ pushd out_last
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
 ${XRAY_SEGPRINT} -zd design.bits >design.segp
 ${XRAY_DIR}/tools/segprint2fasm.py design.segp design.fasm
+${XRAY_DIR}/tools/fasm2frame.py design.fasm design.frm
 popd
