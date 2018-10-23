1360ae7746fdddd69728552315c0ef84689c0b3a
John McMaster <johndmcmaster@gmail.com>
2018-10-23 13:24:59 -0700

    bram: remove obsolete file
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/102-bram-data/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export XRAY_ROI=SLICE_X0Y100:SLICE_X27Y149
-#export XRAY_ROI_FRAMES=0x00820000:0x00820080
-export XRAY_ROI_FRAMES=0x00000000:0xFFFFFFFF
-export XRAY_ROI_GRID_X1="9"
-source $XRAY_DIR/utils/environment.sh
-
