bc65819b7970a3c95152163ece975ce2a792a5aa
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-10-23 15:15:47 -0700

    Run make format.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- minitests/roi_harness/create_design_json.py
+++ minitests/roi_harness/create_design_json.py
@@ -3,8 +3,11 @@ import csv
 import argparse
 import sys
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Creates design.json from output of ROI generation tcl script.")
+    parser = argparse.ArgumentParser(
+        description=
+        "Creates design.json from output of ROI generation tcl script.")
     parser.add_argument('--design_txt', required=True)
     parser.add_argument('--design_info_txt', required=True)
 
@@ -24,6 +27,7 @@ def main():
             j['info'][name] = int(value)
 
     json.dump(j, sys.stdout, indent=2, sort_keys=True)
+
 
 if __name__ == '__main__':
     main()
