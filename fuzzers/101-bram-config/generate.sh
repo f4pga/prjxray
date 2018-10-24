aa9386b6c1938d6925cfd2cdd2d50e99e6f46dde
John McMaster <johndmcmaster@gmail.com>
2018-10-23 23:32:30 -0700

    bram-confg: write all products to build
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/101-bram-config/generate.sh
+++ fuzzers/101-bram-config/generate.sh
@@ -2,15 +2,17 @@
 
 set -ex
 
+FUZDIR=$PWD
 source ${XRAY_GENHEADER}
 
-python3 ../top.py >top.v
-vivado -mode batch -source ../generate.tcl
+
+python3 $FUZDIR/top.py >top.v
+vivado -mode batch -source $FUZDIR/generate.tcl
 test -z "$(fgrep CRITICAL vivado.log)"
 
 for x in design*.bit; do
 	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
 done
 
-python3 ../generate.py
+python3 $FUZDIR/generate.py
 
