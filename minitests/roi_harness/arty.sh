77881252e2e5a3510094ebd51badf2ebd8d60e20
Tim 'mithro' Ansell <me@mith.ro>
2018-10-24 16:36:04 -0700

    minitests/roi_harness: Add XRAY_PIN values valid for Arty.
    Allows generating the fuzzers/001-part-yaml for the Arty part.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- minitests/roi_harness/arty.sh
+++ minitests/roi_harness/arty.sh
@@ -1,3 +1,14 @@
+# XC7A35TICSG324-1L
 export XRAY_PART=xc7a35tcsg324-1
 export XRAY_PINCFG=ARTY-A7-SWBUT
 
+# For generating DB
+export XRAY_PIN_00="G13"
+export XRAY_PIN_01="B11"
+export XRAY_PIN_02="E15"
+export XRAY_PIN_03="U12"
+export XRAY_PIN_04="D13"
+export XRAY_PIN_05="J17"
+export XRAY_PIN_06="U14"
+
+source $XRAY_DIR/utils/environment.sh
