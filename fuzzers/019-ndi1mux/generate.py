92e0724f28ce0e6802237b13656064c6add63b85
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-11-01 16:37:50 -0700

    Rename DI1MUX to be underneith the *LUT.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- fuzzers/019-ndi1mux/generate.py
+++ fuzzers/019-ndi1mux/generate.py
@@ -24,9 +24,9 @@ for l in f:
     c31 = int(c31)
     b31 = int(b31)
     a31 = int(a31)
-    segmk.add_site_tag(loc, "ADI1MUX.AI", 1 ^ a31)
-    segmk.add_site_tag(loc, "BDI1MUX.BI", 1 ^ b31)
-    segmk.add_site_tag(loc, "CDI1MUX.CI", 1 ^ c31)
+    segmk.add_site_tag(loc, "ALUT.DI1MUX.AI", 1 ^ a31)
+    segmk.add_site_tag(loc, "BLUT.DI1MUX.BI", 1 ^ b31)
+    segmk.add_site_tag(loc, "CLUT.DI1MUX.CI", 1 ^ c31)
 
 segmk.compile()
 segmk.write()
