912460e4b69055d64884a336eff6d005ca6326da
John McMaster <johndmcmaster@gmail.com>
2018-11-07 10:10:01 -0800

    fuzzers: consolidate build scripts, move products to build dir
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/013-clbncy0/generate.sh
+++ fuzzers/013-clbncy0/generate.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
 set -ex
-
-source ${XRAY_GENHEADER}
-
-#echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
-
-python3 ../top.py >top.v
-vivado -mode batch -source ../generate.tcl
-test -z "$(fgrep CRITICAL vivado.log)"
-
-for x in design*.bit; do
-	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
-done
-
-python3 ../generate.py
+source ${XRAY_DIR}/utils/top_generate.sh
 
