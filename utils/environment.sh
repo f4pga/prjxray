c22995bf6ca702fa5cfee56493036c55fbad0731
John McMaster <johndmcmaster@gmail.com>
2018-11-13 14:04:26 -0800

    environment.sh: fix PYTHONPATH
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- utils/environment.sh
+++ utils/environment.sh
@@ -1,19 +1,20 @@
+# dirs
 XRAY_ENV_PATH="${BASH_SOURCE[0]}"
 while [ -h "$XRAY_ENV_PATH" ]; do # resolve $XRAY_ENV_PATH until the file is no longer a symlink
   XRAY_UTILS_DIR="$( cd -P "$( dirname "$XRAY_ENV_PATH" )" && pwd )"
   XRAY_ENV_PATH="$(readlink "$XRAY_ENV_PATH")"
   [[ $XRAY_ENV_PATH != /* ]] && XRAY_ENV_PATH="$XRAY_UTILS_DIR/$XRAY_ENV_PATH" # if $XRAY_ENV_PATH was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-export PYTHONPATH="${XRAY_DIR}:${XRAY_DIR}/third_party/fasm:$PYTHONPATH"
 export XRAY_UTILS_DIR="$( cd -P "$( dirname "$XRAY_ENV_PATH" )" && pwd )"
-
 export XRAY_DIR="$( dirname "$XRAY_UTILS_DIR" )"
 export XRAY_DATABASE_DIR="${XRAY_DIR}/database"
 export XRAY_TOOLS_DIR="${XRAY_DIR}/build/tools"
 
+# misc
 export XRAY_PART_YAML="${XRAY_DATABASE_DIR}/${XRAY_DATABASE}/${XRAY_PART}.yaml"
+export PYTHONPATH="${XRAY_DIR}:${XRAY_DIR}/third_party/fasm:$PYTHONPATH"
 
-
+# tools
 export XRAY_GENHEADER="${XRAY_UTILS_DIR}/genheader.sh"
 export XRAY_BITREAD="${XRAY_TOOLS_DIR}/bitread --part_file ${XRAY_PART_YAML}"
 export XRAY_MERGEDB="bash ${XRAY_UTILS_DIR}/mergedb.sh"
