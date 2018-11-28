58e6b50b50b45075a3ef97387b9c197a66610108
John McMaster <johndmcmaster@gmail.com>
2018-11-27 18:50:14 -0800

    rempips: log progress
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/056-rempips/generate.sh
+++ fuzzers/056-rempips/generate.sh
@@ -3,8 +3,8 @@
 FUZDIR=$PWD
 source ${XRAY_GENHEADER}
 
-vivado -mode batch -source ../generate.tcl
+vivado -mode batch -source $FUZDIR/generate.tcl
 
 ${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
-python3 ../generate.py
+python3 $FUZDIR/generate.py
 
