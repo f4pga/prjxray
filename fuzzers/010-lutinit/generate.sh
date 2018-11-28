fa48558c1c01a266935581e929ac5d1ce5b08059
John McMaster <johndmcmaster@gmail.com>
2018-11-27 18:42:55 -0800

    010: build dir
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/010-lutinit/generate.sh
+++ fuzzers/010-lutinit/generate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+FUZDIR=$PWD
 source ${XRAY_GENHEADER}
 
 echo '`define SEED 32'"'h$(echo $1 | md5sum | cut -c1-8)" > setseed.vh
 
-vivado -mode batch -source ../generate.tcl
+vivado -mode batch -source $FUZDIR/generate.tcl
 
 for i in 0 1 2; do
 	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_$i.bits -z -y design_$i.bit
 done
 
 for i in 0 1 2; do
-	python3 ../generate.py $i
+	python3 $FUZDIR/generate.py $i
 done
 
