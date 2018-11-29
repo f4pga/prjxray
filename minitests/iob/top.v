379ed08344e09149cd3885a980e6410afa92f4da
John McMaster <johndmcmaster@gmail.com>
2018-11-28 20:34:02 -0800

    iob minitest
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- minitests/iob/top.v
+++ minitests/iob/top.v
@@ -1,3 +1,16 @@
+/*
+IOBUF
+    Not a primitive?
+    Looks like it has an OBUFT
+
+
+Output buffer family:
+    OBUF
+    OBUFDS
+    OBUFT
+    OBUFTDS
+*/
+
 `ifndef ROI
 ERROR: must set ROI
 `endif
@@ -88,4 +101,83 @@ module roi_io_b(input clk, input [255:0] din, output [255:0] dout);
         .T(1'b1));
 
 endmodule
+
+/*
+For some reason this doesn't diff
+Was this optimized out?
+
+ERROR: [Place 30-69] Instance roi/dut/OBUFT (OBUFT) is unplaced after IO placer
+ERROR: [Place 30-68] Instance roi/dut/OBUFT (OBUFT) is not placed
+*/
+
+/*
+module roi_prop_a(input clk, input [255:0] din, output [255:0] dout);
+    assign dout[0] = din[0] & din[1];
+
+    //(* LOC="D19", KEEP, DONT_TOUCH *)
+    (* KEEP, DONT_TOUCH *)
+    IOBUF #(
+        .DRIVE(8),
+        .IBUF_LOW_PWR("TRUE"),
+        .IOSTANDARD("DEFAULT"),
+        .SLEW("SLOW")
+    ) dut (
+        .O(dout[1]),
+        .I(din[0]),
+        .T(din[1]));
+endmodule
+
+module roi_prop_b(input clk, input [255:0] din, output [255:0] dout);
+    assign dout[0] = din[0] & din[1];
+
+    //(* LOC="D19", KEEP, DONT_TOUCH *)
+    (* KEEP, DONT_TOUCH *)
+    IOBUF #(
+        .DRIVE(12),
+        .IBUF_LOW_PWR("TRUE"),
+        .IOSTANDARD("DEFAULT"),
+        .SLEW("SLOW")
+    ) dut (
+        .O(dout[1]),
+        .I(din[0]),
+        .T(din[1]));
+endmodule
+*/
+
+/*
+ERROR: [DRC REQP-1581] obuf_loaded: OBUFT roi/dut pin O drives one or more invalid loads. The loads are: dout_shr[1]_i_1 
+ERROR: [Place 30-69] Instance roi/dut (OBUFT) is unplaced after IO placer
+hmm
+Abandoning verilog approach
+tcl seems to work well, just use it directly
+*/
+module roi_prop_a(input clk, input [255:0] din, output [255:0] dout);
+    (* LOC="D19", KEEP, DONT_TOUCH *)
+    //(* KEEP, DONT_TOUCH *)
+    OBUFT #(
+        .DRIVE(8),
+        .IOSTANDARD("DEFAULT"),
+        .SLEW("SLOW")
+    ) dut (
+        //.O(dout[1]),
+        .O(),
+        .I(din[0]),
+        .T(din[1]));
+endmodule
+
+module roi_prop_b(input clk, input [255:0] din, output [255:0] dout);
+    (* LOC="D19", KEEP, DONT_TOUCH *)
+    //(* KEEP, DONT_TOUCH *)
+    (* KEEP, DONT_TOUCH *)
+    OBUFT #(
+        .DRIVE(12),
+        .IOSTANDARD("DEFAULT"),
+        .SLEW("SLOW")
+    ) dut (
+        //.O(dout[1]),
+        .O(),
+        .I(din[0]),
+        .T(din[1]));
+endmodule
+
 
