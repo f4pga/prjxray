3fa5df7cb184ae8239675d0bbd374072d095bd36
John McMaster <johndmcmaster@gmail.com>
2018-11-28 19:37:55 -0800

    iob minitest: sweep misc parameters
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

---
+++ minitests/iob/runme_tcl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+: "${PROJECT:?Need to set PROJECT non-empty}"
+
+# Create build dir
+export SRC_DIR=$PWD
+BUILD_DIR=build/$PROJECT
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+export TOP_V=$SRC_DIR/tcl.v
+
+vivado -mode batch -source $SRC_DIR/$PROJECT.tcl
+for x in design*.bit; do
+	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o ${x}s -z -y $x
+done
+test -z "$(fgrep CRITICAL vivado.log)"
+touch run.ok
+
---
+++ minitests/iob/template.tcl
@@ -0,0 +1,20 @@
+# Create a simple design with a few IOs
+
+create_project -force -part $::env(XRAY_PART) design design
+read_verilog $::env(TOP_V)
+synth_design -top top -flatten_hierarchy none
+
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_00) IOSTANDARD LVCMOS33" [get_ports clk]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_01) IOSTANDARD LVCMOS33" [get_ports stb]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_02) IOSTANDARD LVCMOS33" [get_ports di]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_03) IOSTANDARD LVCMOS33" [get_ports do]
+
+set_property CFGBVS VCCO [current_design]
+set_property CONFIG_VOLTAGE 3.3 [current_design]
+set_property BITSTREAM.GENERAL.PERFRAMECRC YES [current_design]
+
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
+
+place_design
+route_design
+
