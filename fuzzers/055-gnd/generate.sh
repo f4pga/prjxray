028e2f78592a1f0231efbdc86458c62680f3538d
John McMaster <johndmcmaster@gmail.com>
2018-12-17 14:49:08 -0800

    055-gnd: QUICK, build dir, simplify .sh
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/055-gnd/generate.sh
+++ fuzzers/055-gnd/generate.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-source ${XRAY_GENHEADER}
-
-vivado -mode batch -source ../generate.tcl
-
-${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design.bits -z -y design.bit
-python3 ../generate.py
+set -ex
+source ${XRAY_DIR}/utils/top_generate.sh
 
