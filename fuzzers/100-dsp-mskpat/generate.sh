7a236be2f360a114b7dcc32bf4c3de7987f83a68
John McMaster <johndmcmaster@gmail.com>
2018-12-18 17:36:31 -0800

    dsp-maskpat: move to build dir
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/100-dsp-mskpat/generate.sh
+++ fuzzers/100-dsp-mskpat/generate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 source ${XRAY_GENHEADER}
 
-vivado -mode batch -source ../generate.tcl
+vivado -mode batch -source $FUZDIR/generate.tcl
 
 for i in {10..29}; do
 	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o design_${i}.bits -z -y design_${i}.bit
-	python3 ../generate.py $i
+	python3 $FUZDIR/generate.py $i
 done
 
