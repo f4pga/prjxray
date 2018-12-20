9b960b0ccd0ac1e1156bdbe8e464c9d0e427dbe7
John McMaster <johndmcmaster@gmail.com>
2018-12-19 18:46:51 -0800

    hclkpips: build dir
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/058-hclkpips/generate.sh
+++ fuzzers/058-hclkpips/generate.sh
@@ -2,11 +2,11 @@
 
 source ${XRAY_GENHEADER}
 
-vivado -mode batch -source ../generate.tcl
+vivado -mode batch -source $FUZDIR/generate.tcl
 
 for x in design_*.bit; do
 	${XRAY_BITREAD} -F $XRAY_ROI_FRAMES -o ${x}s -z -y ${x}
 done
 
-python3 ../generate.py $(ls design_*.bit | cut -f1 -d.)
+python3 $FUZDIR/generate.py $(ls design_*.bit | cut -f1 -d.)
 
