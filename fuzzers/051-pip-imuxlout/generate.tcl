a4b0c6f1dee0ae0d368751c3241258f13d442aca
Karol Gugala <kgugala@antmicro.com>
2019-01-09 22:56:27 +0100

    fuzzers: refactor fuzzer 51
    The fuzzer is now todo files based. It is also using generic
    generate.py script.
    For now the fuzzer instantiates picorv and randluts besides
    the logic under test. This is required for to deliver negative
    examples to segmaker. Because of the size of the design this
    fuzzer is taking really long to complete. We need to find a
    smallest possible design that will be enough to generate the DB.
    
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- fuzzers/051-pip-imuxlout/generate.tcl
+++ fuzzers/051-pip-imuxlout/generate.tcl
@@ -3,10 +3,13 @@ source "$::env(XRAY_DIR)/utils/utils.tcl"
 create_project -force -part $::env(XRAY_PART) design design
 
 read_verilog $::env(FUZDIR)/top.v
+read_verilog $::env(FUZDIR)/picorv32.v
 synth_design -top top
 
-set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_00) IOSTANDARD LVCMOS33" [get_ports i]
-set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_01) IOSTANDARD LVCMOS33" [get_ports o]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_00) IOSTANDARD LVCMOS33" [get_ports clk]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_01) IOSTANDARD LVCMOS33" [get_ports din]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_02) IOSTANDARD LVCMOS33" [get_ports dout]
+set_property -dict "PACKAGE_PIN $::env(XRAY_PIN_03) IOSTANDARD LVCMOS33" [get_ports stb]
 
 create_pblock roi
 resize_pblock [get_pblocks roi] -add "$::env(XRAY_ROI)"
@@ -14,13 +17,14 @@ resize_pblock [get_pblocks roi] -add "$::env(XRAY_ROI)"
 set_property CFGBVS VCCO [current_design]
 set_property CONFIG_VOLTAGE 3.3 [current_design]
 set_property BITSTREAM.GENERAL.PERFRAMECRC YES [current_design]
+
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
 set_param tcl.collectionResultDisplayLimit 0
 
 place_design
 route_design
 
 # write_checkpoint -force design.dcp
-
 
 set fp [open "../../todo.txt" r]
 set todo_lines {}
@@ -29,8 +33,8 @@ for {gets $fp line} {$line != ""} {gets $fp line} {
 }
 close $fp
 
-set int_l_tiles [randsample_list [llength $todo_lines] [filter [pblock_tiles roi] {TYPE == INT_L}]]
-set int_r_tiles [randsample_list [llength $todo_lines] [filter [pblock_tiles roi] {TYPE == INT_R}]]
+set int_l_tiles [filter [pblock_tiles roi] {TYPE == INT_L}]]
+set int_r_tiles [filter [pblock_tiles roi] {TYPE == INT_R}]]
 
 for {set idx 0} {$idx < [llength $todo_lines]} {incr idx} {
     set line [lindex $todo_lines $idx]
@@ -51,17 +55,36 @@ for {set idx 0} {$idx < [llength $todo_lines]} {incr idx} {
     set src_prefix [regsub {(.*/.).*} ${clb_src_pin} {\1}]
     set dst_prefix [regsub {(.*/.).*} ${clb_dst_pin} {\1}]
 
-    if {$src_prefix == $dst_prefix} {
-        set slice [get_sites -of_objects $clb_dst_pin]
-        set lut [regsub {.*/} $src_prefix {}]6LUT
+    set slice [get_sites -of_objects $clb_dst_pin]
+    set src_slice [get_sites -of_objects $clb_src_pin]
+    set lut [regsub {.*/} $src_prefix {}]6LUT
+    set dff [regsub {.*/} $src_prefix {}]FF
 
-        puts "=== $slice $lut ($clb_src_pin -> $clb_dst_pin)"
+    set mynet [create_net mynet_$idx]
+    set mylut [create_cell -reference LUT1 mylut_$idx]
+    set lutin [regsub {.*(.)} $clb_dst_pin {A\1}]
+    set_property -dict "LOC $slice BEL $lut LOCK_PINS I0:$lutin" $mylut
 
-        set mynet [create_net mynet_$idx]
-        set mylut [create_cell -reference LUT1 mylut_$idx]
-        set lutin [regsub {.*(.)} $clb_dst_pin {A\1}]
-        set_property -dict "LOC $slice BEL $lut LOCK_PINS I0:$lutin" $mylut
+    # some source wires may be FF outputs, in such cases
+    # we need to place and route an FF
+
+    set src_type [regsub {.*/*(.$)} $clb_src_pin {\1}]
+    if { $src_type == "Q" } {
+        set mydff [create_cell -reference FDCE mydff_$idx]
+        set_property -dict "LOC $src_slice BEL $dff" $mydff
+        connect_net -net $mynet -objects "$mylut/I0 $mydff/Q"
+    } else {
         connect_net -net $mynet -objects "$mylut/I0 $mylut/O"
+    }
+
+    set route_list "$tile/$src_wire $tile/$dst_wire"
+    puts "route_via $mynet $route_list"
+    set rc [route_via $mynet $route_list 0]
+    if {$rc != 0} {
+        puts "SUCCESS"
+    } else {
+        puts "Manual routing failed"
+        # TODO: We should probably fail here
     }
 }
 
