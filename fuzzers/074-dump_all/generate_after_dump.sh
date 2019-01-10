19a93121a6b45a3236eeb2d0445b8b86b771fc85
Mehdi Khairy <mehdi.khairy@more-magic.org>
2019-01-10 23:57:35 +0100

    Bug #171: Fixing build directory for Fuzzers 074
    Signed-off-by: Mehdi Khairy <mehdi.khairy@more-magic.org>

--- fuzzers/074-dump_all/generate_after_dump.sh
+++ fuzzers/074-dump_all/generate_after_dump.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -xe
 
-rm -rf output
-mkdir -p output
+rm -rf build/output
+mkdir -p build/output
 python3 reduce_tile_types.py \
-  --root_dir specimen_001/ \
-  --output_dir output
+  --root_dir build/specimen_001/ \
+  --output_dir build/output
 python3 create_node_tree.py \
-  --dump_all_root_dir specimen_001/ \
-  --ordered_wires_root_dir ../072-ordered_wires/specimen_001/ \
-  --output_dir output
-python3 reduce_site_types.py --output_dir output
-python3 generate_grid.py --root_dir specimen_001/ --output_dir output \
+  --dump_all_root_dir build/specimen_001/ \
+  --ordered_wires_root_dir ../072-ordered_wires/build/specimen_001/ \
+  --output_dir build/output
+python3 reduce_site_types.py --output_dir build/output
+python3 generate_grid.py --root_dir build/specimen_001/ --output_dir build/output \
   --ignored_wires ${XRAY_DATABASE}_ignored_wires.txt
