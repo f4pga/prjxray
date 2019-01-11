85ff7fe55c0f2fd18b820cc9783911cfd819fedf
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-01-10 17:04:05 -0800

    Run make format.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- utils/bit2fasm.py
+++ utils/bit2fasm.py
@@ -11,6 +11,7 @@ from prjxray import bitstream
 import subprocess
 import tempfile
 
+
 def bit_to_bits(bitread, part_yaml, bit_file, bits_file, frame_range=None):
     """ Calls bitread to create bits (ASCII) from bit file (binary) """
     if frame_range:
@@ -18,8 +19,10 @@ def bit_to_bits(bitread, part_yaml, bit_file, bits_file, frame_range=None):
     else:
         frame_range_arg = ''
 
-    subprocess.check_output('{} --part_file {} {} -o {} -z -y {}'.format(
-        bitread, part_yaml, frame_range_arg, bits_file, bit_file), shell=True)
+    subprocess.check_output(
+        '{} --part_file {} {} -o {} -z -y {}'.format(
+            bitread, part_yaml, frame_range_arg, bits_file, bit_file),
+        shell=True)
 
 
 def bits_to_fasm(db_root, bits_file, verbose, canonical):
@@ -63,9 +66,14 @@ def main():
         default_bitread = os.path.join(os.getenv("XRAY_TOOLS_DIR"), 'bitread')
 
     parser.add_argument('--db-root', help="Database root.", **db_root_kwargs)
-    parser.add_argument('--part', help="Name of part being targetted.", **part_kwargs)
-    parser.add_argument('--bitread', help="Name of part being targetted.", default=default_bitread)
-    parser.add_argument('--frame_range', help="Frame range to use with bitread.")
+    parser.add_argument(
+        '--part', help="Name of part being targetted.", **part_kwargs)
+    parser.add_argument(
+        '--bitread',
+        help="Name of part being targetted.",
+        default=default_bitread)
+    parser.add_argument(
+        '--frame_range', help="Frame range to use with bitread.")
     parser.add_argument('bit_file', help='')
     parser.add_argument(
         '--verbose',
@@ -77,14 +85,15 @@ def main():
 
     with tempfile.NamedTemporaryFile() as bits_file:
         bit_to_bits(
-                bitread=args.bitread,
-                part_yaml=os.path.join(args.db_root, '{}.yaml'.format(args.part)),
-                bit_file=args.bit_file,
-                bits_file=bits_file.name,
-                frame_range=args.frame_range,
-                )
+            bitread=args.bitread,
+            part_yaml=os.path.join(args.db_root, '{}.yaml'.format(args.part)),
+            bit_file=args.bit_file,
+            bits_file=bits_file.name,
+            frame_range=args.frame_range,
+        )
 
-        bits_to_fasm(args.db_root, bits_file.name, args.verbose, args.canonical)
+        bits_to_fasm(
+            args.db_root, bits_file.name, args.verbose, args.canonical)
 
 
 if __name__ == '__main__':
