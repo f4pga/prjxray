2927f1c572923a0f53775af360b66b748be4163f
Tim 'mithro' Ansell <me@mith.ro>
2019-02-06 18:27:34 +1100

    Don't remove the database.
    Why was the download-latest-db.sh doing a clone and then removing all
    the stuff it just cloned?
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- download-latest-db.sh
+++ download-latest-db.sh
@@ -4,11 +4,5 @@ GITHUB_PROTO=${1:-https}
 GITHUB_URL=$GITHUB_PROTO://github.com/SymbiFlow/prjxray-db.git
 rm -rf database
 git clone $GITHUB_URL database
-# Causes confusion if you try to commit in DB dir
-# But doesn't effect most people, probably leave as is
-# rm -rf database/.git
-# travis, .gitignore, etc
-rm -f database/* 2>/dev/null
-rm -f database/.* 2>/dev/null
-# Restore settings files so fuzzers run correctly
+# Update files in the database from our version so fuzzers run correctly.
 git checkout HEAD database
