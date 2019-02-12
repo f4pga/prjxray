2311f251497e22596beae021cd5bd5a4e2e40f9f
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-02-12 10:55:41 -0800

    Add initial BUFG config bits.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ fuzzers/042-clk-bufg-config/generate.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import json
+
+from prjxray.segmaker import Segmaker
+
+
+def main():
+    segmk = Segmaker("design.bits")
+
+    print("Loading tags")
+    with open('params.json') as f:
+        params = json.load(f)
+
+    for row in params:
+        base_name = 'BUFGCTRL_X{}Y{}'.format(row['x'], row['y'])
+
+        segmk.add_site_tag(
+            row['site'], '{}.IN_USE'.format(base_name), row['IN_USE'])
+
+        if not row['IN_USE']:
+            continue
+
+        for param in (
+                'INIT_OUT',
+                'IS_IGNORE0_INVERTED',
+                'IS_IGNORE1_INVERTED',
+                ):
+            segmk.add_site_tag(
+                row['site'], '{}.{}'.format(base_name, param), row[param])
+
+        if row['connect0'] and row['connect1']:
+            for param in (
+                    'PRESELECT_I0',
+                    ):
+                segmk.add_site_tag(
+                    row['site'], '{}.Z{}'.format(base_name, param), 1 ^ row[param])
+
+            for param in (
+                    'PRESELECT_I1',
+                    ):
+                segmk.add_site_tag(
+                    row['site'], '{}.{}'.format(base_name, param), row[param])
+
+        if row['connect0']:
+            for param, tag in (
+                    ('IS_CE0_INVERTED', 'ZINV_CE0'),
+                    ('IS_S0_INVERTED', 'ZINV_S0')
+                    ):
+                segmk.add_site_tag(
+                    row['site'], '{}.{}'.format(base_name, tag), 1 ^ row[param])
+
+        if row['connect1']:
+            for param, tag in (
+                    ('IS_CE1_INVERTED', 'ZINV_CE1'),
+                    ('IS_S1_INVERTED', 'ZINV_S1')
+                    ):
+                segmk.add_site_tag(
+                    row['site'], '{}.{}'.format(base_name, tag), 1 ^ row[param])
+
+
+    segmk.compile()
+    segmk.write()
+
+
+if __name__ == '__main__':
+    main()
