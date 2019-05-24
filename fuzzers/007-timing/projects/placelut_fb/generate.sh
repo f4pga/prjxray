306b40eebbb927d8ced0de814e070d9ceaa7f183
Maciej Kurc <mkurc@antmicro.com>
2019-05-24 15:05:53 +0200

    Changed all scripts to use explicitly python3
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- fuzzers/007-timing/projects/placelut_fb/generate.sh
+++ fuzzers/007-timing/projects/placelut_fb/generate.sh
@@ -3,7 +3,7 @@
 set -ex
 source ../generate.sh
 
-python ../generate.py --sdx 4 --sdy 4  >top.v
+python3 ../generate.py --sdx 4 --sdy 4  >top.v
 ${XRAY_VIVADO} -mode batch -source ../generate.tcl
 timing_txt2csv
 
