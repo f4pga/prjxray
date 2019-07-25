5409992f033dbc4a2a968c9f0686de999e620f24
Alessandro Comodi <acomodi@antmicro.com>
2019-07-25 18:57:03 +0200

    make format
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- fuzzers/036-iob-ologic/generate.py
+++ fuzzers/036-iob-ologic/generate.py
@@ -65,7 +65,8 @@ def main():
                     if d['{}_USED'.format(opt)]:
                         k = 'IS_{}_INVERTED'.format(opt)
                         segmk.add_site_tag(site, k, d[k])
-                        segmk.add_site_tag(site, 'ZINV_{}'.format(opt), 1 ^ d[k])
+                        segmk.add_site_tag(
+                            site, 'ZINV_{}'.format(opt), 1 ^ d[k])
 
                 for idx in range(4):
                     k = 'IS_T{}_INVERTED'.format(idx + 1)
