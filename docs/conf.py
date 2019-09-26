c30f75542384a5c81dbe15a343521b9983f0a7cc
Robert Winkler <rwinkler@antmicro.com>
2019-09-26 11:58:55 +0200

    Change theme to Sphinx Material Design
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -113,13 +113,67 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_symbiflow_theme'
+html_theme = 'sphinx_materialdesign_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # Specify a list of menu in Header.
+    # Tuples forms:
+    #  ('Name', 'external url or path of pages in the document', boolean, 'icon name')
+    #
+    # Third argument:
+    # True indicates an external link.
+    # False indicates path of pages in the document.
+    #
+    # Fourth argument:
+    # Specify the icon name.
+    # For details see link.
+    # https://material.io/icons/
+    'header_links': [
+        ('Home', 'index', False, 'home'),
+        ("GitHub", "https://github.com/SymbiFlow/prjxray", True, 'link')
+    ],
+
+    # Customize css colors.
+    # For details see link.
+    # https://getmdl.io/customize/index.html
+    #
+    # Values: amber, blue, brown, cyan deep_orange, deep_purple, green, grey, indigo, light_blue,
+    #         light_green, lime, orange, pink, purple, red, teal, yellow(Default: indigo)
+    'primary_color':
+    'deep_purple',
+    # Values: Same as primary_color. (Default: pink)
+    'accent_color':
+    'purple',
+
+    # Customize layout.
+    # For details see link.
+    # https://getmdl.io/components/index.html#layout-section
+    'fixed_drawer':
+    True,
+    'fixed_header':
+    True,
+    'header_waterfall':
+    True,
+    'header_scroll':
+    False,
+
+    # Render title in header.
+    # Values: True, False (Default: False)
+    'show_header_title':
+    False,
+    # Render title in drawer.
+    # Values: True, False (Default: True)
+    'show_drawer_title':
+    True,
+    # Render footer.
+    # Values: True, False (Default: True)
+    'show_footer':
+    True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
