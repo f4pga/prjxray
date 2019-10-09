624de250e87ae57d2adbc0aa22e7d8da9f934613
Jake Mercer <jmercer04@qub.ac.uk>
2019-10-09 20:55:39 +0100

    FUZZER - DSP - Cleared Bits
    Changed some tags to be prefixed with 'Z'; these bits are cleared and need the prefix to indicate
    the inversion so that they are resolved to the DB correctly.
    
    Signed-off-by: Jake Mercer <jmercer04@qub.ac.uk>

--- fuzzers/100-dsp-mskpat/generate.py
+++ fuzzers/100-dsp-mskpat/generate.py
@@ -12,24 +12,27 @@ with open('params.csv', 'r') as f:
 
         a_input = str(d['a_input'])
         segmk.add_site_tag(
-            d['site'], "%s.A_INPUT[0]" % (dsp),
-            (1 if a_input == "DIRECT" else 0))
+            d['site'], "%s.ZA_INPUT[0]" % (dsp),
+            (0 if a_input == "DIRECT" else 1))
 
         b_input = str(d['b_input'])
         segmk.add_site_tag(
-            d['site'], "%s.B_INPUT[0]" % (dsp),
-            (1 if b_input == "DIRECT" else 0))
+            d['site'], "%s.ZB_INPUT[0]" % (dsp),
+            (0 if b_input == "DIRECT" else 1))
 
         autoreset_patdet = str(d['autoreset_patdet'])
         if autoreset_patdet == "RESET_MATCH":
             segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[0]" % (dsp), 0)
-            segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[1]" % (dsp), 0)
+            segmk.add_site_tag(
+                d['site'], "%s.ZAUTO_RESET_PATDET[1]" % (dsp), 1)
         elif autoreset_patdet == "NO_RESET":
             segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[0]" % (dsp), 0)
-            segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[1]" % (dsp), 1)
+            segmk.add_site_tag(
+                d['site'], "%s.ZAUTO_RESET_PATDET[1]" % (dsp), 0)
         elif autoreset_patdet == "RESET_NOT_MATCH":
             segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[0]" % (dsp), 1)
-            segmk.add_site_tag(d['site'], "%s.AUTO_RESET_PATDET[1]" % (dsp), 0)
+            segmk.add_site_tag(
+                d['site'], "%s.ZAUTO_RESET_PATDET[1]" % (dsp), 1)
 
         mask = int(d['mask'])
         pattern = int(d['pattern'])
