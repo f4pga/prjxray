0ec9df0f14c93650c6e425a12583518ea35c3aca
Alessandro Comodi <acomodi@antmicro.com>
2019-12-11 13:59:48 +0100

    zynq: Update settings to use only bottom right of device
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- settings/zynq7.sh
+++ settings/zynq7.sh
@@ -1,5 +1,5 @@
 export XRAY_DATABASE="zynq7"
-export XRAY_PART="xc7z010clg400-1"
+export XRAY_PART="xc7z020clg484-1"
 export XRAY_ROI_FRAMES="0x00000000:0xffffffff"
 
 # All CLB's in part, all BRAM's in part, all DSP's in part.
@@ -10,21 +10,18 @@ export XRAY_EXCLUDE_ROI_TILEGRID=""
 export XRAY_IOI3_TILES="RIOI3_X31Y9"
 
 # These settings must remain in sync
-export XRAY_ROI="SLICE_X00Y50:SLICE_X43Y99 RAMB18_X0Y20:RAMB18_X2Y39 RAMB36_X0Y10:RAMB36_X2Y19 IOB_X0Y50:IOB_X0Y99"
+export XRAY_ROI="SLICE_X0Y0:SLICE_X49Y49 RAMB18_X0Y0:RAMB18_X2Y19 RAMB36_X0Y0:RAMB36_X2Y9 IOB_X0Y0:IOB_X0Y49 DSP48_X0Y0:DSP48_X2Y19"
 
 # Most of CMT X0Y2.
-export XRAY_ROI_GRID_X1="83"
-export XRAY_ROI_GRID_X2="118"
+export XRAY_ROI_GRID_X1="0"
+export XRAY_ROI_GRID_X2="86"
 # Include VBRK / VTERM
-export XRAY_ROI_GRID_Y1="0"
-export XRAY_ROI_GRID_Y2="51"
+export XRAY_ROI_GRID_Y1="105"
+export XRAY_ROI_GRID_Y2="155"
 
-export XRAY_PIN_00="L14"
-export XRAY_PIN_01="L15"
-export XRAY_PIN_02="M14"
-export XRAY_PIN_03="M15"
-export XRAY_PIN_04="K16"
-export XRAY_PIN_05="J16"
-export XRAY_PIN_06="J15"
+export XRAY_PIN_00="Y9"
+export XRAY_PIN_01="U10"
+export XRAY_PIN_02="N17"
+export XRAY_PIN_03="P18"
 
 source $(dirname ${BASH_SOURCE[0]})/../utils/environment.sh
