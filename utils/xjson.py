3c91c98e03ae2013f563727d68694779e8760958
Tim 'mithro' Ansell <me@mith.ro>
2020-02-09 23:17:56 -0600

    Fix the sorting stage.
     - Rework how the json files are sorted (numbers are treated as
       numerics).
     - Sort csv and txt files.
     - Sort segbits.*origin_info.db files.
     - Sort the grid file.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- utils/xjson.py
+++ utils/xjson.py
@@ -4,6 +4,8 @@ import json
 import re
 import sys
 
+from collections import OrderedDict
+
 
 def extract_numbers(s):
     """
@@ -31,23 +33,48 @@ def sort(data):
         data.sort(key=lambda o: (o['tile_types'], o['grid_deltas']))
     else:
 
-        def walker(o, f):
-            if isinstance(o, dict):
-                for i in o.values():
-                    walker(i, f)
+        def key(o):
+            if o is None:
+                return None
+            elif isinstance(o, str):
+                return extract_numbers(o)
+            elif isinstance(o, int):
+                return o
             elif isinstance(o, list):
+                return [key(i) for i in o]
+            elif isinstance(o, dict):
+                return [(key(k), key(v)) for k, v in o.items()]
+            raise ValueError(repr(o))
+
+        def rsorter(o):
+            if isinstance(o, dict):
+                nitems = []
+                for k, v in o.items():
+                    nitems.append((key(k), k, rsorter(v)))
+                nitems.sort(key=lambda n: n[0])
+
+                new_dict = OrderedDict()
+                for _, k, v in nitems:
+                    new_dict[k] = v
+                return new_dict
+
+            elif isinstance(o, list):
+                if len(o) == 2:
+                    return o
+
+                nlist = []
                 for i in o:
-                    walker(i, f)
-            f(o)
+                    nlist.append((key(i), rsorter(i)))
+                nlist.sort(key=lambda n: n[0])
 
-        def f(o):
-            if isinstance(o, list):
-                if len(o) > 2:
-                    strings = all(isinstance(x, str) for x in o)
-                    if strings:
-                        o.sort()
+                new_list = []
+                for _, i in nlist:
+                    new_list.append(i)
+                return new_list
+            else:
+                return o
 
-        walker(data, f)
+        return rsorter(data)
 
 
 def pprint(f, data):
@@ -55,8 +82,8 @@ def pprint(f, data):
     if not isinstance(f, io.TextIOBase):
         detach = True
         f = io.TextIOWrapper(f)
-    sort(data)
-    json.dump(data, f, sort_keys=True, indent=4)
+    data = sort(data)
+    json.dump(data, f, indent=4)
     f.write('\n')
     f.flush()
     if detach:
