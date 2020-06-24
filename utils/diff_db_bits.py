4a2672fe55483430697863f953874581a5603be5
Robert Winkler <rwinkler@antmicro.com>
2020-06-24 12:48:54 +0200

    Improve diffs generated by diff_db_bits.py
    This commit introduces additional value sorting for
    each segbits file line during diff generation.
    It also adds the flag that forces diff tool to produce a minimal diff.
    
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- utils/diff_db_bits.py
+++ utils/diff_db_bits.py
@@ -15,6 +15,29 @@ import os.path
 import glob
 
 
+def sort_and_save_to_file(in_file_path, out_file):
+    with open(in_file_path) as in_db_file:
+        in_lines = in_db_file.read().splitlines()
+
+        # Sort file lines
+        in_lines.sort()
+
+        # Sort values in each line
+        for line in in_lines:
+            elems = line.split(' ')
+            if elems[1].startswith("origin"):  # segbits.orgin_info.db
+                str_part = elems[:2]
+                values = elems[2:]
+            else:  # segbits.db
+                str_part = elems[:1]
+                values = elems[1:]
+            values.sort()
+
+            # Save the sorted line to the output file
+            str_line = "{} {}\n".format(" ".join(str_part), " ".join(values))
+            out_file.write(str_line)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=
@@ -35,17 +58,16 @@ def main():
             print('{} not found!'.format(b_db_in))
             continue
 
-        with tempfile.NamedTemporaryFile(suffix="_a_{}".format(
+        with tempfile.NamedTemporaryFile(mode='w', suffix="_a_{}".format(
                 a_db_base)) as a_db_out, tempfile.NamedTemporaryFile(
-                    suffix="_b_{}".format(a_db_base)) as b_db_out:
-            subprocess.check_call(
-                "sort {}".format(a_db_in), shell=True, stdout=a_db_out)
-            subprocess.check_call(
-                "sort {}".format(b_db_in), shell=True, stdout=b_db_out)
+                    mode='w', suffix="_b_{}".format(a_db_base)) as b_db_out:
+
+            sort_and_save_to_file(a_db_in, a_db_out)
+            sort_and_save_to_file(b_db_in, b_db_out)
 
             print("Comparing {}".format(a_db_base))
             subprocess.call(
-                "diff -U 0 {} {}".format(a_db_out.name, b_db_out.name),
+                "diff -U 0 -d {} {}".format(a_db_out.name, b_db_out.name),
                 shell=True)
 
 
