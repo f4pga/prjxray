5245999dc407aa19bd3618144b7d71bb4973f488
Alessandro Comodi <acomodi@antmicro.com>
2021-01-18 15:48:15 +0100

    063-gtp-common-conf: add license to pushdb.sh
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- fuzzers/063-gtp-common-conf/pushdb.sh
+++ fuzzers/063-gtp-common-conf/pushdb.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Copyright (C) 2017-2020  The Project X-Ray Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
 
 if ! test $(find . -name "segdata_gtp_common_mid_right.txt" | wc -c) -eq 0
 then
