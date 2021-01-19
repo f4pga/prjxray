50553b0c17eac007f8bc8f45b92dd1a97387f1e7
Alessandro Comodi <acomodi@antmicro.com>
2021-01-19 11:31:21 +0100

    063-gtp-common-conf: fix Makefile to clean only current part
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- fuzzers/063-gtp-common-conf/pushdb.sh
+++ fuzzers/063-gtp-common-conf/pushdb.sh
@@ -7,16 +7,16 @@
 #
 # SPDX-License-Identifier: ISC
 
-if ! test $(find . -name "segdata_gtp_common_mid_right.txt" | wc -c) -eq 0
+if ! test $(find ${BUILD_DIR} -name "segdata_gtp_common_mid_right.txt" | wc -c) -eq 0
 then
-    ${XRAY_MERGEDB} gtp_common_mid_right build/segbits_gtp_common.db
-    ${XRAY_MERGEDB} mask_gtp_common_mid_right build/mask_gtp_common.db
-    ${XRAY_MERGEDB} gtp_common_mid_left build/segbits_gtp_common.db
-    ${XRAY_MERGEDB} mask_gtp_common_mid_left build/mask_gtp_common.db
+    ${XRAY_MERGEDB} gtp_common_mid_right ${BUILD_DIR}/segbits_gtp_common.db
+    ${XRAY_MERGEDB} mask_gtp_common_mid_right ${BUILD_DIR}/mask_gtp_common.db
+    ${XRAY_MERGEDB} gtp_common_mid_left ${BUILD_DIR}/segbits_gtp_common.db
+    ${XRAY_MERGEDB} mask_gtp_common_mid_left ${BUILD_DIR}/mask_gtp_common.db
 fi
 
-if ! test $(find . -name "segdata_gtp_common.txt" | wc -c) -eq 0
+if ! test $(find ${BUILD_DIR} -name "segdata_gtp_common.txt" | wc -c) -eq 0
 then
-    ${XRAY_MERGEDB} gtp_common build/segbits_gtp_common.db
-    ${XRAY_MERGEDB} mask_gtp_common build/mask_gtp_common.db
+    ${XRAY_MERGEDB} gtp_common ${BUILD_DIR}/segbits_gtp_common.db
+    ${XRAY_MERGEDB} mask_gtp_common ${BUILD_DIR}/mask_gtp_common.db
 fi
