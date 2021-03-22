8305cc81d958d020b819635a64fe9b63bb3cba63
Alessandro Comodi <acomodi@antmicro.com>
2021-03-22 10:38:24 +0100

    run make format-tcl
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- utils/update_resources.tcl
+++ utils/update_resources.tcl
@@ -24,6 +24,6 @@ foreach bank [split $banks " "] {
 set fp [open $::env(TMP_FILE) w]
 
 puts $fp "{"
-puts $fp "\t\"clk_pins\": \"$clk_pins\","
-puts $fp "\t\"data_pins\": \"$data_pins\""
+    puts $fp "\t\"clk_pins\": \"$clk_pins\","
+    puts $fp "\t\"data_pins\": \"$data_pins\""
 puts $fp "}"
