f7f473014c6a900f0ddb28054d81407ddadcb61a
Karol Gugala <kgugala@antmicro.com>
2022-05-18 14:06:29 +0200

    CI: xilinx: fix license condition
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- .github/workflows/scripts/xilinx.sh
+++ .github/workflows/scripts/xilinx.sh
@@ -56,7 +56,7 @@ echo "----------------------------------------"
 
 # Create a tunnel to the server which has the Xilinx licenses and port forward
 # them.
-if [ ! -z "$GHA_SSH_TUNNEL_CONFIG_SECRET_NAME" ]; then
+if [[ ! -z "$GHA_SSH_TUNNEL_CONFIG_SECRET_NAME" ]]; then
 
 	echo
 	echo "Xilinx license server ssh key found, checking the license"
