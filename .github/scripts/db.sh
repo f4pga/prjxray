4b163fe35800203361f344903cba891b5f6aeea1
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-18 03:59:19 +0200

    ci: mv workflows/scripts/ scripts/
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/scripts/db.sh
+++ .github/scripts/db.sh
@@ -9,7 +9,31 @@
 
 set -e
 
-source $(dirname "$0")/hostinfo.sh
+echo
+echo "========================================"
+echo "Host Environment"
+echo "----------------------------------------"
+export
+echo "----------------------------------------"
+
+echo
+echo "========================================"
+echo "Host CPU"
+echo "----------------------------------------"
+export CORES=$(nproc --all)
+echo "Cores: $CORES"
+echo
+echo "Memory"
+echo "----------------------------------------"
+cat /proc/meminfo
+echo "----------------------------------------"
+export MEM_GB=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo)/(1024*1024)))
+echo "Total Memory (GB): $MEM_GB"
+
+# Approx memory per grid process
+export MEM_PER_RUN=8
+export MAX_GRID_CPU=$(($MEM_GB/$MEM_PER_RUN))
+export MAX_VIVADO_PROCESS=$(($MEM_GB/$MEM_PER_RUN))
 
 echo
 echo "======================================="
